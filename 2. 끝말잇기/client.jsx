36b0b5bde4a3ec3af798c47b0fb7818952420d42
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-05 14:17:05 +0900

    webpack 에러 해결

--- 2. 끝말잇기/client.jsx
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-// wordRelay 컴퍼넌트 가져오기
-const WordRelay = require('./WordRelay');
-
-ReactDOM.render(<WordRelay/>), document.querySelector('#root');
