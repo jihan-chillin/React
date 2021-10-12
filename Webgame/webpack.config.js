1b6a89e9dc63e22aa0a7f3666b8d8a59dd5b6fee
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-12 18:31:25 +0900

    module system & webpack setting

---
+++ Webgame/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports ={
+    name : 'word-relay-setting', 
+    mode : 'development', // 실서비스 : production, 
+    devtool : 'eval', // 빠르게
+    resolve : {
+        extensions : ['.js', '.jsx']
+    },
+    entry  : {
+        app : ['./client'],
+    },
+
+    output : {
+        // 현재 폴더 안에 있는 dist라는 폴더도
+        // webpack.config.js 파일의 세팅값이 적용되도록 하는 것
+        path : path.join(__dirname, 'dist'),
+        filename : 'app.js'
+    },
+}
