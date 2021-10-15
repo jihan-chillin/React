1d05fa822533ce15da140147c883e5b8fa1fa5dd
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-15 15:22:23 +0900

    babel/preset-env 설정 -> 브라우저 버전 호환

--- Gugudan/webpack.config.js
+++ Gugudan/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { webpack } = require('webpack');
 
 module.exports = {
     mode : 'development',
@@ -14,11 +15,22 @@ module.exports = {
             test : /\jsx?$/,
             loader : 'babel-loader',
             options : {
-                presets : ['@babel/preset-env', '@babel/preset-react'],
+                presets : [
+                    ['@babel/preset-env', {
+                        targets : {
+                            browsers : ['> 5% in KR','last 2 chrome versions'], //이전 chrome 버전도 호환가능
+                        },
+                    }], 
+                    '@babel/preset-react',
+                ],
                 plugins : ['@babel/plugin-proposal-class-properties'],
             },
         }],
     },
+    // plugins : [
+    //     // loader의 옵션으로 debug를 true화
+    //     new webpack.LoaderOptionsPlugin({debug : true}),
+    // ],
     output : {
         filename : 'app.js',
         path : path.join(__dirname, 'dist')
