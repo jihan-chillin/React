4b4aec67f93ab5d78dc6cab6661ab05f523a75e0
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-31 18:12:47 +0900

    Class로 숫자야구 만들기 ( React에서  array.push를 쓰면 안되는 이유  )

--- NumBaseball/NumberBaseball.jsx
+++ NumBaseball/NumberBaseball.jsx
@@ -3,6 +3,14 @@ import Try from './Try';
 
 function getNumbers(){
     // 숫자 4개를 중복없이 랜덤하게 뽑는 함수
+    const Candidates = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for(let i = 0 ; i< 4 ; i++){
+        const chosen = Candidates.splice(Math.floor(Math.random()*(9-i)), 1)[0];
+        array.push(chosen);
+    }
+
+    return array;
 }
 
 class NumberBaseball extends Component{
@@ -13,24 +21,58 @@ class NumberBaseball extends Component{
         tries : [],
     };
 
-    fruits = [
-        {fruit : "사과", taste : "맛있다"},
-        {fruit : "바나나", taste : "맛앖다"},
-        {fruit : "포도", taste : "맛있다"},
-        {fruit : "귤", taste : "맛있다"},
-        {fruit : "감", taste : "없어서 못먹음"},
-        {fruit : "배", taste : "비싸서 못먹음"},
-        {fruit : "밤", taste : "구우면 맛있지"},
+    onSubmitForm = (e) =>{
+        e.prevendDefault();
+        // 입력한 값과 정답이 같으면
+        if(this.state.value === this.state.answer.join('')){
+            this.setState({
+                result : '홈런!',
+                tries : [...this.state.tries, {try : this.state.value, result : '홈런!'}],
+            })
+            alert('게임을 다시 시작합니다.');
+            this.setState({
+                value : '',
+                answer : getNumbers(),
+                tries : [],
+            });
 
-    ];
-
-    onSubmitForm = () =>{
-
+        }else{ // 답이 틀렸으면, 
+            const anwerArray = this.state.value.split('').map((v)=> parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if(this.state.tries.length >= 9){ // 10번 이상 틀렸을 때, 
+                this.setState({
+                    result : `10번 넘게 틀려서 실패! 답은 ${answer.join('.')}였습니다.`,
+                });
+                alert('게임을 다시 시작합니다.');
+                this.setState({
+                    value : '',
+                    answer : getNumbers(),
+                    tries : [],
+                });
+            }else{ // 10 번 이내로 틀렸을 때, 
+                for(let i = 0 ; i< 4 ; i ++){
+                    if(answerArray[i] === this.state.answer[i]){
+                        strike += 1;
+                    }else if(this.state.answer.includes(answerArray[i])){
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries : [...this.state.tries, {try : this.state.value, result : `${strike}, ${ball}`}],
+                    value : '',
+                })
+            }
+        }
+        console.log(this.state.value);
     };
 
-    onChangeInput = () => {
-
-    };
+    onChangeInput = (e) =>{
+        console.log(this.state.answer);
+        this.setState({
+            value : e.target.value,
+        })
+    }
 
     render(){
         return(
@@ -42,9 +84,9 @@ class NumberBaseball extends Component{
                 <div>시도 : {this.state.tries.length}</div>
                 <ul>
                     {/* 3. key로 반복문 하는 방법 : 객체로 */}
-                    {this.fruits.map((v, i)=>{
+                    {this.state.tries.map((v, i)=>{
                         return(
-                         <Try key={v.fruit + v.taste} value={v} index={i}/>
+                         <Try key={`${i + 1}차시도 : `} tryInfo={v} index={i}/>
                         )
                     })}
                    
