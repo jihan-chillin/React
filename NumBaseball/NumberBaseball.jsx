bfcd1f2f993ea56c2e8cd7840c4528466bb99f96
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-31 17:26:12 +0900

    컴포넌트 분리 ( Props )

--- NumBaseball/NumberBaseball.jsx
+++ NumBaseball/NumberBaseball.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import Try from './Try';
 
 function getNumbers(){
-    // 숫자 4개를 랜덤하게 뽑는 함수
+    // 숫자 4개를 중복없이 랜덤하게 뽑는 함수
 }
 
 class NumberBaseball extends Component{
@@ -11,6 +12,17 @@ class NumberBaseball extends Component{
         answer : getNumbers(),
         tries : [],
     };
+
+    fruits = [
+        {fruit : "사과", taste : "맛있다"},
+        {fruit : "바나나", taste : "맛앖다"},
+        {fruit : "포도", taste : "맛있다"},
+        {fruit : "귤", taste : "맛있다"},
+        {fruit : "감", taste : "없어서 못먹음"},
+        {fruit : "배", taste : "비싸서 못먹음"},
+        {fruit : "밤", taste : "구우면 맛있지"},
+
+    ];
 
     onSubmitForm = () =>{
 
@@ -29,11 +41,13 @@ class NumberBaseball extends Component{
                 </form>
                 <div>시도 : {this.state.tries.length}</div>
                 <ul>
-                    {["사과", "바나나", "포도"].map((v)=>{
+                    {/* 3. key로 반복문 하는 방법 : 객체로 */}
+                    {this.fruits.map((v, i)=>{
                         return(
-                            <li>{v}</li>
+                         <Try key={v.fruit + v.taste} value={v} index={i}/>
                         )
                     })}
+                   
                 </ul>
             </>
         )
