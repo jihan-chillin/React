8871ff824b992efe695d2d2155d8823e27ddfd30
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-31 21:21:37 +0900

    숫자야구 Hooks로 전환

--- NumBaseball/NumberBaseball.jsx
+++ NumBaseball/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Try from './Try';
 
 function getNumbers(){
@@ -9,48 +9,36 @@ function getNumbers(){
         const chosen = Candidates.splice(Math.floor(Math.random()*(9-i)), 1)[0];
         array.push(chosen);
     }
-
     return array;
 }
 
-class NumberBaseball extends Component{
-    state ={
-        result :'',
-        value : '',
-        answer : getNumbers(),
-        tries : [],
-    };
+const NumberBaseball = () =>{
+    const [result, setResult] = useState('');
+    const [answer, setAnswer] = useState('');
+    const [tries, setTries] = useState('');
 
-    onSubmitForm = (e) =>{
-        const {result, value, tries, answer} = this.state;
+const onSubmitForm = (e) =>{
         e.prevendDefault();
         // 입력한 값과 정답이 같으면
         if(value === answer.join('')){
-            this.setState({
-                result : '홈런!',
-                tries : [...tries, {try : value, result : '홈런!'}],
-            })
-            alert('게임을 다시 시작합니다.');
-            this.setState({
-                value : '',
-                answer : getNumbers(),
-                tries : [],
+            setResult('홈런!');
+            setTries((prevTries)=>{
+                return [...prevTries, {try : value, result : '홈런!'}]
             });
-
+            alert('게임을 다시 시작합니다.');
+            setValue('');
+            setAnswer(getNumbers());
+            setTries([]);
         }else{ // 답이 틀렸으면, 
             const anwerArray = value.split('').map((v)=> parseInt(v));
             let strike = 0;
             let ball = 0;
             if(tries.length >= 9){ // 10번 이상 틀렸을 때, 
-                this.setState({
-                    result : `10번 넘게 틀려서 실패! 답은 ${answer.join('.')}였습니다.`,
-                });
+                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join('.')}였습니다.`);
                 alert('게임을 다시 시작합니다.');
-                this.setState({
-                    value : '',
-                    answer : getNumbers(),
-                    tries : [],
-                });
+                setValue('');
+                setAnswer(getNumbers());
+                setTries([]);
             }else{ // 10 번 이내로 틀렸을 때, 
                 for(let i = 0 ; i< 4 ; i ++){
                     if(answerArray[i] === answer[i]){
@@ -59,44 +47,34 @@ class NumberBaseball extends Component{
                         ball += 1;
                     }
                 }
-                this.setState({
-                    tries : [...tries, {try : value, result : `${strike}, ${ball}`}],
-                    value : '',
-                })
+                // 옛날 tries로 현재 tries로 만들기 때문에 함수형으로 만들어줘야 함!
+                setTries((prevTries)=>[...prevTries, {try : value, result : `${strike}, ${ball}`}]);
+                setValue('');            
             }
         }
         console.log(value);
     };
 
-    onChangeInput = (e) =>{
-        console.log(answer);
-        this.setState({
-            value : e.target.value,
-        })
+const onChangeInput = (e) =>{
+        setValue(e.target.value);
     }
-
-    render(){
-        // 구조분해 
-        const {result, value, tries} = this.state
-        return(
-            <>
-                <h1>{result}</h1>
-                <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value={value} onChange={this.onChangeInput}/>
-                </form>
-                <div>시도 : {tries.length}</div>
-                <ul>
-                    {/* 3. key로 반복문 하는 방법 : 객체로 */}
-                    {tries.map((v, i)=>{
-                        return(
-                         <Try key={`${i + 1}차시도 : `} tryInfo={v} index={i}/>
-                        )
-                    })}
-                   
-                </ul>
-            </>
-        )
-    }
+    return(
+        <>
+            <h1>{result}</h1>
+            <form onSubmit={this.onSubmitForm}>
+                <input maxLength={4} value={value} onChange={this.onChangeInput}/>
+            </form>
+            <div>시도 : {tries.length}</div>
+            <ul>
+                {/* 3. key로 반복문 하는 방법 : 객체로 */}
+                {tries.map((v, i)=>{
+                    return(
+                     <Try key={`${i + 1}차시도 : `} tryInfo={v} index={i}/>
+                    )
+                })}
+               
+            </ul>
+        </>
+    )
 }
-
 export default NumberBaseball;
--- NumBaseball/Try.jsx
+++ NumBaseball/Try.jsx
@@ -1,15 +1,11 @@
 import React, {Component} from 'react';
 
-class Try extends Component {
-    render() {
-        const {tryInfo} = this.state;
-        return (
-            <li>
-                <div>{tryInfo.try}</div>
-                <div>{tryInfo.result}</div>
-            </li>
-        )
-    }
+const Try = ({tryInfo}) =>{
+    return (
+        <li>
+            <div>{tryInfo.try}</div>
+            <div>{tryInfo.result}</div>
+        </li>
+    )
 }
-
 export default Try;
