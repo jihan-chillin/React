2bc3fb2b963f9154e44d1217c7a106d44790c9a9
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-11-08 21:03:10 +0900

    가위바위보 게임 만들기

---
+++ RSP/client.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import ResponseCheck from './ResponseCheck';
+// import NumberBaseball from './RenderTest';
+import RSP from './RSP';
+
+ReactDOM.render(<RSP />, document.querySelector('#root'));
